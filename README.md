5ab87967f5297ad9fa3d5d426e52f296dc8947ff
hbenyazi <hamza.ben.yazid@oracle.com>
2024-06-22 20:47:14 +0100

    First commit

--- README.md
+++ README.md
@@ -1,96 +1,45 @@
 # Obsidian Sample Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+The Live Variables Plugin for Obsidian helps you manage and reuse data within your notes by allowing you to define variables in your note's properties. When you change the value of a variable, all occurrences of that variable in your note are automatically updated. This ensures consistency and can save you time when working with frequently changing information.
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+## Features
+- Dynamic Variable Management: Define variables in your note's properties and update them with ease. All instances of the variable in your note will update automatically.
+- Seamless Integration: Fully integrated into the Obsidian environment, enhancing your note-taking experience.
+- Efficiency Boost: Reduce the need for manual updates of repeated information.
+- User-Friendly Interface: Intuitive setup and management of variables.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+# Usage
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+1. Install the Plugin:
 
-## First time developing plugins?
+- Download the plugin from the GitHub repository.
+- Place the plugin folder in your Obsidian plugins directory.
+- Enable the plugin in the Obsidian settings.
 
-Quick starting guide for new plugin devs:
+2. Define Variables:
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+- Open the note where you want to use variables.
+- In the note's properties, define your variables. For example
+  ```
+  ---
+    projectName: Obsidian Live Variables Plugin
+    dueDate: 2024-12-31
+  ---
+  ```
 
-## Releasing new releases
+3. Use Variables in Your Note:
+You can referance a variable with the comand `Live Variable: Insert live variable` and then choose you variable.
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+4. Update Variables:
+Change the value of the variables in the note's properties, and all occurrences of the variables in the note will be updated automatically.
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+## Demo
+To see the plugin in action, check out the following demo:
 
-## Adding your plugin to the community plugin list
+![Demo Gif](demo/demo.gif)
 
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+## Contributing
+Contributions are welcome! If you have suggestions for improvements or new features, please open an issue or submit a pull request.
 
-## How to use
-
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+## License
+This project is licensed under the MIT License
--- main.ts
+++ main.ts
@@ -1,81 +1,80 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, FrontMatterCache, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, SuggestModal } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface LiveVariableSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: LiveVariableSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class LiveVariable extends Plugin {
+	properties: FrontMatterCache | undefined;
+
+	changedProperty = (newProperties: FrontMatterCache | undefined) => {
+		for (let [newPropKey, newPropVal] of Object.entries(newProperties ?? {})) {
+			let currentPropVal = this.properties?.[newPropKey];
+			if (currentPropVal !== newPropVal) {
+				return [newPropKey, newPropVal];
+			}
+		}
+		return undefined;
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		// initialize properties
+		this.app.workspace.on("file-open", (file)=>{
+			if(file){
+				this.properties = this.app.metadataCache.getFileCache(file)?.frontmatter;
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
+		})
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'insert-live-variable',
+			name: 'Insert Live Variable',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (view.file) {
+					this.properties = this.app.metadataCache.getFileCache(view.file)?.frontmatter;
+				}
+				let file = view.file;
+				if (file) {
+					new PropertySelectionModal(
+						this.app,
+						this.properties ?? {},
+						(property) => {
+							editor.replaceSelection(`<span id="${property.key}">${property.value}</span>`);
+							new Notice(`Variable ${property.key} inserted`);
+						}).open();
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEvent(
+			this.app.metadataCache.on("changed", (path, data, cache) => {
+				let frontmatterProperties = cache.frontmatter
+				if (!this.properties) {
+					this.properties = frontmatterProperties;
+					return
+				}
+				let changedProperty = this.changedProperty(frontmatterProperties)
+				if (changedProperty) {
+					let key = changedProperty[0]
+					let newValue = changedProperty[1]
+					new Notice(`property ${key} changed to ${newValue}. Resolving all references...`);
+					let file = this.app.vault.getFileByPath(path.path);
+					if (file) {
+						let re = new RegExp(String.raw`<span id="${key}">.+</span>`, "g")
+						data = data.replace(re, `<span id="${key}">${newValue}</span>`)
+						this.app.vault.modify(file, data)
+					}
+					this.properties = frontmatterProperties;
+				}
+			}),
+		);
 	}
 
 	onunload() {
@@ -83,52 +82,42 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+interface Property {
+	key: string;
+	value: string;
+}
+
+export class PropertySelectionModal extends SuggestModal<Property> {
+	onSelect: (property: Property) => any
+	properties: FrontMatterCache
+
+	constructor(app: App, properties: FrontMatterCache, onSelect: (property: Property) => any) {
 		super(app);
+		this.properties = properties;
+		this.onSelect = onSelect;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	// Returns all available suggestions.
+	getSuggestions(query: string): Property[] {
+		return Object.entries(this.properties).filter((property) =>
+			property[0].toLowerCase().includes(query.toLowerCase())
+		).map(entry => ({ key: entry[0], value: entry[1] }));
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	// Renders each suggestion item.
+	renderSuggestion(property: Property, el: HTMLElement) {
+		el.createEl("div", { text: property.key });
+		el.createEl("small", { text: property.value });
 	}
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	// Perform action on the selected suggestion.
+	onChooseSuggestion(property: Property, evt: MouseEvent | KeyboardEvent) {
+		this.onSelect(property);
 	}
 }
